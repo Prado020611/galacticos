d2d9ffdf18ee84350003158ee30c6cdfb64b1a9a
CrisCastro10 <rojascastrocristian91@gmail.com>
2025-07-17 16:03:36 -0700

    Add files via upload
    Este es el nuevo morros

---
+++ Clases/Cocina.java
@@ -0,0 +1,46 @@
+package Clases;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cris Castro
+ */
+public class Cocina {
+    public int id;
+    public String nombre;
+
+    public Cocina(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre.toString();
+    }
+    
+    
+    
+}
+
+
---
+++ Clases/Conexion.java
@@ -0,0 +1,28 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+import java.sql.*;
+/**
+ *
+ * @author Cris Castro
+ */
+public class Conexion {
+    
+    public Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    public Conexion () {
+    try {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/baseproyecto", "root", "");
+        System.out.println("Conectado");
+    } catch (Exception e){
+        System.out.println("Error al conectar");// +e.getMessage());
+    //con = null;
+    }
+    }
+}
+
